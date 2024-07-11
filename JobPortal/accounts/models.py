06514055d9aac4089708ad2ed2a1993821dbd901
Abdullah00110 <148894144+Abdullah00110@users.noreply.github.com>
2024-07-12 01:08:36 +0530

    Add models and settings

--- JobPortal/accounts/models.py
+++ JobPortal/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+
+class UserManger(BaseUserManager):
+    def create_user(self, email, name, tc, password = None, password2 = None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError(:)
