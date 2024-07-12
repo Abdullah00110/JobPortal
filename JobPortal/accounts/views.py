99e3f7f89611102567aaba847cc4f1e8bbf702c3
Abdullah00110 <148894144+Abdullah00110@users.noreply.github.com>
2024-07-13 00:41:37 +0530

    Serializer, viwes, urls

--- JobPortal/accounts/views.py
+++ JobPortal/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from accounts.serializers import *
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return{
+        
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),    
+    }
+    
+class Register(APIView):
+    def get(self, request):
+        return Response({"Message" : "PLEASE REGISTER YOURSELF"})
+    
+    def post(self, request):
+        serializer = RegisterSerializer(data = request.data)
