fd5c59ec1bba05b94e149b46096411b2b307ea7f
Abdullah00110 <148894144+Abdullah00110@users.noreply.github.com>
2024-07-12 20:41:57 +0530

    Models, settings

--- JobPortal/accounts/admin.py
+++ JobPortal/accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from accounts.models import *
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class UserModelAdmin(UserAdmin):
+    list_display = ["email" , "name" , "tc", "is_admin"]
+    list_filter = ["is_admin"]
+    fieldsets = [
+        
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["name", "tc"]}),
+        ("Permission", {"fields":["is_admin"]})
+    ]
+    
+    add_fieldsets = [
+        
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields" : ["email", "name", "tc", "password", "password2"],
+            },
+        ),
+    ]
+    
+    search_fields = ["email"]
+    ordering = ["email", "name"]
+    filter_horizontal = []
+    
+    
+admin.site.register(User, UserModelAdmin)
+    
